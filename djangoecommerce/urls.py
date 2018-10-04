bd17c4a1604af286eeda8750da6b0c8ee6243c1b
Allex Vissoci <allex.vissoci@gmail.com>
2018-10-04 20:30:36 -0300

    removing duplicated admin url

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     url(r'^contato/$', views.contact, name='contact'),
     url(r'^produto/$', views.product, name='product'),
     url(r'^produtos/$', views.product_list, name='product_list'),
-    url(r'^admin/', admin.site.urls),
 ]
