c1abbecf21fe0ba6ce9b720f3e5f24dd788fe7cd
Allex Vissoci <allex.vissoci@gmail.com>
2018-10-04 23:29:46 -0300

    add models and views tests

---
+++ apps/catalog/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.urls import reverse
+from model_mommy import mommy
+
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        self.category = mommy.make('catalog.Category')
+
+    def test_get_absolute_url(self):
+        self.assertEquals(
+            self.category.get_absolute_url(),
+            reverse('catalog:category', kwargs={'slug': self.category.slug})
+        )
+
+
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+        self.product = mommy.make('catalog.Product', slug='produto')
+
+    def test_get_absolute_url(self):
+        self.assertEquals(
+            self.product.get_absolute_url(),
+            reverse('catalog:product', kwargs={'slug': 'produto'})
+        )
