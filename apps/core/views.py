e6ef718bf6d5222cc1e283dd25a9e2bd1767e104
Allex Vissoci <allex.vissoci@gmail.com>
2018-10-06 12:02:19 -0300

    refactor core view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,10 +1,15 @@
 # coding=utf-8
 from django.shortcuts import render
 from .forms import ContactForm
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
+
+
+index = IndexView.as_view()
 
 
 def contact(request):
