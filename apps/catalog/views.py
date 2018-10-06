e67f5cf0e47296b200e0b55d1f098b3feed72969
Allex Vissoci <allex.vissoci@gmail.com>
2018-10-06 12:22:20 -0300

    refactor catalog view

--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -1,26 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.views import generic
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductsListView(generic.ListView):
+
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'product_list'
 
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, 'catalog/category.html', context)
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        slug = self.kwargs['slug']
+        context['current_category'] = get_object_or_404(Category, slug=slug)
+        return context
 
 
-def product(request, slug):
-    product = Product.objects.get(slug=slug)
-    context = {
-        'product': product,
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductListView(generic.ListView):
+
+    template_name = 'catalog/product.html'
+    context_object_name = 'product'
+
+    def get_queryset(self):
+        return Product.objects.get(slug=self.kwargs['slug'])
+
+
+product_list = ProductsListView.as_view()
+category = CategoryListView.as_view()
+product = ProductListView.as_view()
